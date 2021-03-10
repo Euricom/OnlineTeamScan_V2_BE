b3778d7bec25aea164e4d5273228fe9425de77ea
VincentHertens <55692053+VincentHertens@users.noreply.github.com>
2021-03-10 15:43:49 +0100

    Revert "Revert "Add entities and configurations""
    This reverts commit 9a113e54d3df9ad54caf17b4ab372e562bb2b40d.

--- OnlineTeamScan_V2_BE/DAL/Models/TeamMember.cs
+++ OnlineTeamScan_V2_BE/DAL/Models/TeamMember.cs
@@ -9,11 +9,10 @@ namespace DAL.Models
     public class TeamMember
     {
         public int Id { get; set; }
-        public int TeamId { get; set; }
         public string Email { get; set; }
-        public string Firstname { get; set; }
-        public string Lastname { get; set; }
-        public bool IsActive { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int TeamId { get; set; }
 
         public Team Team { get; set; }
     }
