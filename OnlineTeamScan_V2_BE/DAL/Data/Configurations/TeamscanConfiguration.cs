be83e1e65b2669ba8880c8d3475171544f82c251
VincentHertens <55692053+VincentHertens@users.noreply.github.com>
2021-03-10 15:49:27 +0100

    Revert "Revert "Revert "Add entities and configurations"""
    This reverts commit b3778d7bec25aea164e4d5273228fe9425de77ea.

--- OnlineTeamScan_V2_BE/DAL/Data/Configurations/TeamscanConfiguration.cs
+++ OnlineTeamScan_V2_BE/DAL/Data/Configurations/TeamscanConfiguration.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using AutoMapper;
+using DAL.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +10,31 @@ using System.Threading.Tasks;
 
 namespace DAL.Data.Configurations
 {
-    public class TeamscanConfiguration
+    public class TeamscanConfiguration : Profile, IEntityTypeConfiguration<Teamscan>
     {
+        public TeamscanConfiguration()
+        {
+
+        }
+        public void Configure(EntityTypeBuilder<Teamscan> builder)
+        {
+            builder.ToTable("tbl_teamscans");
+            builder.Property(t => t.Id).HasColumnName("id").ValueGeneratedOnAdd();
+            builder.Property(t => t.StartedById).HasColumnName("startedby_id").IsRequired();
+            builder.Property(t => t.TeamId).HasColumnName("team_id").IsRequired();
+            builder.Property(t => t.Title).HasColumnName("title").HasColumnType("varchar(50)").IsRequired();
+            builder.Property(t => t.StartDate).HasColumnName("start_date").HasColumnType("date").IsRequired();
+            builder.Property(t => t.EndDate).HasColumnName("end_date").HasColumnType("date");
+            builder.Property(t => t.ScoreTrust).HasColumnName("score_trust").HasColumnType("decimal(3,2)");
+            builder.Property(t => t.ScoreConflict).HasColumnName("score_conflict").HasColumnType("decimal(3,2)");
+            builder.Property(t => t.ScoreCommitment).HasColumnName("score_commitment").HasColumnType("decimal(3,2)");
+            builder.Property(t => t.ScoreAccountability).HasColumnName("score_accountability").HasColumnType("decimal(3,2)");
+            builder.Property(t => t.ScoreResults).HasColumnName("score_results").HasColumnType("decimal(3,2)");
+
+            builder.HasKey(t => t.Id).IsClustered();
+            builder.HasOne(t => t.Team).WithMany().HasForeignKey(f => f.TeamId).IsRequired();
+            builder.HasOne(t => t.StartedBy).WithMany().HasForeignKey(f => f.StartedBy).IsRequired();
+            builder.HasIndex(t => new { t.TeamId, t.Title }).IsUnique();
+        }
     }
 }
