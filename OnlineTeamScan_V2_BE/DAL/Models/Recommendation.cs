85dbdce5231baaa987d37e482797c2064b84f947
VincentHertens <55692053+VincentHertens@users.noreply.github.com>
2021-04-27 11:28:40 +0200

    Add recommendations

---
+++ OnlineTeamScan_V2_BE/DAL/Models/Recommendation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Models
+{
+    public class Recommendation
+    {
+        public int Id { get; set; }
+        public int DysfunctionId { get; set; }
+        public string Link { get; set; }
+
+        public Dysfunction Dysfunction { get; set; }
+    }
+}
